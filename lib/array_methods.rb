c0bd10499445807ee0f8245773df6756c4d5fd5c
David Jaworski <jaws360@gmail.com>
2019-10-15 00:59:18 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -38,6 +38,9 @@ def find_min_value(array)
     counter += 1
   end  
 end
+
+find_min_value([9999, 1972, 9999999999999999, 1])
+
 #maxNum defined as the first element in list
 #maxNum = array[0]
 #loop through array and test whether the element is bigger than the current maxNum
